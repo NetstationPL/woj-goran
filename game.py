c12cb8193d3585ba1dab3457b57dc7b981b1bb5a
Climbus <climbus@gmail.com>
2015-05-16 09:18:46 +0200

    Ekran

---
+++ game.py
@@ -0,0 +1,20 @@
+import pygame
+from pygame.locals import *
+
+screen_size = (1600, 800)
+
+class Game(object):
+    def __init__(self):
+        pygame.init()
+        pygame.mixer.init()
+        self.surface = pygame.display.set_mode(screen_size, pygame.HWSURFACE)
+        self.gamestate = 1        
+
+    def main(self):
+        while self.gamestate==1:
+            for event in pygame.event.get():
+                if event.type==QUIT or (event.type==KEYDOWN and event.key==K_ESCAPE):
+                    self.gamestate=0
+
+if __name__ == "__main__":
+    Game().main()
