85c97e912d2aded5487598c3e2bc277e3b54826d
pstankiewicz <pi@raspberrypi.(none)>
2015-05-16 09:26:57 +0200

    poprawki flake8 i mniejszcza rozdzielczość

--- game.py
+++ game.py
@@ -1,20 +1,21 @@
 import pygame
-from pygame.locals import *
+from pygame.locals import *  # noqa
 
-screen_size = (1600, 800)
+screen_size = (1024, 798)
+
 
 class Game(object):
     def __init__(self):
         pygame.init()
         pygame.mixer.init()
         self.surface = pygame.display.set_mode(screen_size, pygame.HWSURFACE)
-        self.gamestate = 1        
+        self.gamestate = 1
 
     def main(self):
-        while self.gamestate==1:
+        while self.gamestate == 1:
             for event in pygame.event.get():
-                if event.type==QUIT or (event.type==KEYDOWN and event.key==K_ESCAPE):
-                    self.gamestate=0
+                if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                    self.gamestate = 0
 
 if __name__ == "__main__":
     Game().main()
