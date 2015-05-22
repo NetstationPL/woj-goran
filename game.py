547f97c28fd3d7e2cff839801b79d5f5288d4baa
Climbus <climbus@gmail.com>
2015-05-22 23:21:34 +0200

    fizyki ciag dalszy

--- game.py
+++ game.py
@@ -6,12 +6,12 @@ import pymunk.pygame_util
 
 from player import Player
 
-screen_size = (1024, 798)
+screen_size = (1280, 720)
 
 def create_line(space):
     body = pymunk.Body()
-    body.position = (0, 798)
-    line_shape = pymunk.Segment(body, (0, -0), (1024, 0), 128)
+    body.position = (0, screen_size[1])
+    line_shape = pymunk.Segment(body, (0, -0), (screen_size[0], 0), 128)
     line_shape.elasticity = 0.5
     space.add(line_shape)
     return line_shape
@@ -32,7 +32,7 @@ class Game(object):
         self.players.add(self.player)
         self.space.add(self.player.body, self.player.shape)
         self.clock = pygame.time.Clock()
-        self.STEP = 5
+        
         self.floor = create_line(self.space)
 
     def initial_background(self):
@@ -40,10 +40,10 @@ class Game(object):
         background = background.convert()
         background.fill((128, 128, 250))
         self.surface.blit(background, (0, 0))
-        image = pygame.image.load('images/2.png').convert()
+        image = pygame.image.load('images/land.png').convert()
 
-        for i in range(0, 1024, 128):
-            self.surface.blit(image, (i, 798 - 128))
+        for i in range(0, screen_size[0], image.get_width()):
+            self.surface.blit(image, (i, screen_size[1] - image.get_height()))
 
 
     def main(self):
@@ -51,16 +51,17 @@ class Game(object):
             for event in pygame.event.get():
                 if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
                     self.gamestate = 0
-            self.player.walk = 0
+            
+            self.player.walk(0)
+
             keys = pygame.key.get_pressed()
 
-            #self.player.body.velocity = pymunk.vec2d.Vec2d(0, 0)
             if keys[K_LEFT]:
-                self.player.walk = self.STEP * -1
+                self.player.walk(-1)
             if keys[K_RIGHT]:
-                self.player.walk = self.STEP
+                self.player.walk()
             if keys[K_UP]:
-                self.player.body.velocity = pymunk.vec2d.Vec2d(0, -100)
+                self.player.jump()
 
             self.players.update()
             self.initial_background()
@@ -68,7 +69,7 @@ class Game(object):
             
             #pymunk.pygame_util.flip_y = False            
             #pymunk.pygame_util.draw(self.surface, self.space)
-                
+
             pygame.display.flip()
             self.clock.tick(60)
             self.space.step(1/60.0)
--- player.py
+++ player.py
@@ -7,6 +7,8 @@ class Player(pygame.sprite.Sprite):
     image = pygame.image.load("images/player_idle.png")
     images_str = ["images/player_idle.png", "images/player_walk1.png", "images/player_walk2.png", "images/player_walk3.png", ]
     default_groups = []
+    STEP = 5
+    CHANGE_ANIM_AFTER_STEPS = 5
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self, self.default_groups)
@@ -14,13 +16,17 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 100
         self.rect.y = 100
-        self.walk = 0
+  
+        self.jumping = False
+
         self.frame = 0
         self.images = []
         for i in self.images_str:
             self.images.append(pygame.image.load(i))
         self.frames_count = 0
-        self.CHANGE_ANIM_AFTER_STEPS = 5
+
+        self.walk_delta = 0
+
         self.direction = 1
         self.body = pymunk.Body(30, pymunk.moment_for_box(50, 100, 128))
         self.body.position = (self.rect.centerx, 768 - self.rect.centery)
@@ -29,7 +35,7 @@ class Player(pygame.sprite.Sprite):
     def update(self):
         pygame.sprite.Sprite.update(self)
 
-        if self.walk:
+        if self.is_walking():
             self.frames_count += 1
             # obsługa zmiany klatki animacji po kilku krokach
             if self.frames_count >= self.CHANGE_ANIM_AFTER_STEPS:
@@ -39,7 +45,7 @@ class Player(pygame.sprite.Sprite):
                 self.frames_count = 0
 
             # ustawiamy kierunek poruszania się postaci
-            if self.walk > 0:
+            if self.body.velocity[0] > 0:
                 self.direction = 1
             else:
                 self.direction = -1
@@ -49,6 +55,25 @@ class Player(pygame.sprite.Sprite):
         # odbicie lustrzane klatki animacji jeśli kierunek jest odpowiedni
         if self.direction == -1:
             self.image = pygame.transform.flip(self.image, True, False)
-        self.rect.x += self.walk
-
+        
+        self.rect.centerx = self.body.position[0]
         self.rect.bottom = self.body.position[1] + 40
+
+    def is_jumping(self):
+        if self.body.velocity[1] != 0:
+            return True
+        return False
+
+    def jump(self):
+        if self.is_jumping():
+            return
+        self.body.apply_impulse(pymunk.vec2d.Vec2d(0, -4000))
+
+    def walk(self, direction=1):
+        if not self.is_jumping():
+            self.body.velocity = pymunk.vec2d.Vec2d(500 * direction, 0)
+
+    def is_walking(self):
+        if self.body.velocity[1] == 0 and self.body.velocity[0] != 0:
+            return True
+        return False
